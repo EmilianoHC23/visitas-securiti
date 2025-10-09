b6b8cd15875ed72d463a53c08605c9f9ea0d3273
EmilianoHC23 <22202002@utfv.edu.mx>
2025-10-09 14:23:13 -0600

    style(frontend): restore Bootstrap styles by adding dependency and re-importing CSS in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
